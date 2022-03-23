a15ef6876c8c083a409923cb1ad53bfd89a7bbb1
GonzaloBernardini <gbernardini58@gmail.com>
2022-03-23 11:00:24 -0300

    FirstCommit

---
+++ Shared/News.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace shared
+{
+    public class News
+    {
+        //Identificador Unico por equipo y se genera de manera automatica.
+        public Guid NewsId {get;set;} = Guid.NewGuid();
+
+        public string Description {get;set;}
+
+        public string Paragraph {get;set;}
+
+        public string Image {get;set;}
+
+        public string Url {get;set;}
+    }
+}
